6748ab91b8012acfc24c0202c3c18d1c52645c0c
Yichuan Ma <yichuanma@Yichuans-MacBook-Pro.local>
2021-11-10 14:54:31 -0500

    year of entry

--- yichuanma95/extract_data.py
+++ yichuanma95/extract_data.py
@@ -2,21 +2,22 @@ import pandas as pd
 
 def extract_data(start, end):
     end += 1
-    dfs = []
-    for year in range(2012, 2019+1):
-        dfYear = pd.read_excel('../../data/Brazilian Immigrants.xlsx',
-            sheet_name=str(year))
-        subDf = dfYear[start:end]
-        subDf = subDf.rename(columns={'Unnamed: 0': 'Type', 'State_Code': 'Type',
-            'State_Code.1': 'State Code'}, errors='ignore')
-        cols = subDf.columns.tolist()
-        cols.insert(0, 'Year')
-        subDf = subDf.reindex(columns=cols)
-        years = [year] * (end - start)
-        subDf['Year'] = years
-        dfs.append(subDf)
+    dfs = [generate_subdf(year, start, end) for year in range(2012, 2019+1)]
     df = pd.concat(dfs)
     df.drop(columns=['Unnamed: 49', 'Unnamed: 50', 'Unnamed: 51',
         'Unnamed: 52'], inplace=True)
     df.fillna(0, inplace=True)
     return df
+
+def generate_subdf(year, start, end):
+    dfYear = pd.read_excel('../data/Brazilian Immigrants.xlsx',
+            sheet_name=str(year))
+    subDf = dfYear[start:end]
+    subDf = subDf.rename(columns={'Unnamed: 0': 'Type', 'State_Code': 'Type',
+            'State_Code.1': 'State Code'}, errors='ignore')
+    cols = subDf.columns.tolist()
+    cols.insert(0, 'Year')
+    subDf = subDf.reindex(columns=cols)
+    years = [year] * (end - start)
+    subDf['Year'] = years
+    return subDf
